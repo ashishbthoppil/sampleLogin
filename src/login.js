bd31a466a5ade790cfd5b34ada229d3fc10f5f71
ashishbthoppil <ashishbabythoppil@gmail.com>
2020-10-12 12:52:48 +0530

    further changes

--- src/login.js
+++ src/login.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import { Card } from 'react-bootstrap';
 
 class Login extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+           scrollerClass : "animScroller",
+        };
+    }
+    
     cardStyle = {
         height : '300px',
+    }
+
+    showReg = (e) => {
+      console.log('asd');
     }
 
     render(){
         return(
             <div className="col-12 col-md-6 m-auto">
-                <div className="animScroller">
+                <div id="scroller" className={this.state.scrollerClass}>
                     <h1></h1>
                 </div>
                  <Card style={this.cardStyle}>
@@ -19,11 +31,11 @@ class Login extends Component{
                             <input type="text" className="form-control" placeholder="Username" style={{textAlign:"center"}}/>
                         </div>
                         <div className="inputDiv">
-                            <input type="text" className="form-control" placeholder="Password" style={{textAlign:"center"}}/> 
+                            <input type="password" className="form-control" placeholder="Password" style={{textAlign:"center"}}/> 
                         </div>  
                         <div className="loginBtnDiv">
                             <button className="btn btn-success loginBtn">Login</button>
-                            <button style={{marginLeft:"10px",width:"13em"}} className="btn btn-primary">New User? <span className="fa fa-arrow-right"></span></button>
+                            <button style={{marginLeft:"10px",width:"14em"}} className="btn btn-primary" onClick={this.showReg.bind(this)}>New User? <span className="fa fa-arrow-right"></span></button>
                         </div> 
                       </Card.Body>
                    </Card>
